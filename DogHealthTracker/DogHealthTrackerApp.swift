2dc302cc5af5265bd13dbc6108501c01a16cd361
Maliha Shahzadi <maliha.wiva@gmail.com>
2026-03-09 10:51:36 +0500

    Initial Commit

---
+++ DogHealthTracker/DogHealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogHealthTrackerApp.swift
+//  DogHealthTracker
+//
+//  Created by Maliha on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct DogHealthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
